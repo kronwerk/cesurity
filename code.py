c5395e20ee6dcc92f8dd362ee99fbc04f09fade1
root <root@ubuntu.(none)>
2015-02-27 17:34:11 +0300

    working filemanager

--- code.py
+++ code.py
@@ -1,4 +1,6 @@
 import web
+import cgi
+import sys
 import view, config
 from view import render
 
@@ -7,8 +9,14 @@ urls = (
 )
 
 class index:
-    def GET(self, name):
-        return render.base(view.listing(name))
+    def GET(self, query):
+        query = web.ctx.query
+        print 'input index:', query
+        if query:
+            qdict = cgi.parse_qs(query[1:])
+        else:
+            qdict = {'id': 1, 'left_key': 1, 'right_key': sys.maxint, 'level': 1}
+        return render.base(view.listing(**qdict))
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 import config
 
 def listing(**k):
-    return config.DB.select('my_tree', **k)
+    #return config.DB.select('my_tree', **k)
+    print k
+    return config.DB.query("SELECT * FROM my_tree WHERE left_key >= $left_key AND right_key <= $right_key AND level = $level+1 ORDER BY left_key", vars=k)
