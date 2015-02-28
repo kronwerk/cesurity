063f006c54a9340d4ab14863bf5c9b26c5a68b18
kronwerk <tetragon@mail.ru>
2015-02-28 15:43:49 +0300

    navigation

--- code.py
+++ code.py
@@ -8,14 +8,45 @@ urls = (
     '/(.*)', 'index'
 )
 
+
+class path:
+    def __init__(self):
+        self.nodes = []
+
+    def add_node(self, obj):
+        print "to_node", obj
+        return obj
+
+    def is_empty(self):
+        return len(self.nodes == 0)
+
+    def root_only(self):
+        return len(self.nodes == 1)
+
+    def get(self):
+        if self.root_only():
+            return self.nodes[-1]
+        else:
+            return self.nodes.pop()
+
+
 class index:
+    def __init__(self):
+        self.path = path()
+
     def GET(self, query):
         query = web.ctx.query
         print 'input index:', query
         if query:
             qdict = cgi.parse_qs(query[1:])
+            self.path.add_node(qdict)
         else:
-            qdict = {'id': 1, 'left_key': 1, 'right_key': sys.maxint, 'level': 1}
+            if self.path.is_empty():
+                qdict = config.DB.query("SELECT * FROM my_tree WHERE level = 1")
+                self.path.add_node(qdict)
+            else:
+                qdict = self.path.get()
+        print "GET", qdict, self.path
         return render.base(view.listing(**qdict))
 
 if __name__ == "__main__":
